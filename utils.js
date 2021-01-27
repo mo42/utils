8ec37ee7ccc2eafd66006d88bf3050ac7538c71e
Moritz Knabben <me@mo42.de>
2021-01-27 17:52:11 +0100

    Simplify syntax

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 const u = (function () {
     return {
-        id: function (d) { return d }
-        not: function (f) { return function (d) { return !f(d) } }
+        id: d => d,
+        not: f => d => !f(d)
     }
 })()
